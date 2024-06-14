f5c627760cb3f25279ecbeea43e742d08583ac64
채상희 <chaesanghui142@gmail.com>
2024-06-14 15:25:21 +0900

    c

---
+++ src/main/java/com/nhnacademy/apigateway/config/RouteLocatorConfig.java
@@ -0,0 +1,25 @@
+package com.nhnacademy.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Configuration
+public class RouteLocatorConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(RouteLocatorConfig.class);
+
+    @Bean
+    public RouteLocator myRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("AUTH-SERVER",
+                        p->p.path("/auth/**").and()
+                                .uri("lb://AUTH-SERVER")
+                )
+                .build();
+    }
+}
