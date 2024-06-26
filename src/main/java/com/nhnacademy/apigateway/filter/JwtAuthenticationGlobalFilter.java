1df52f79fb50d049fe684a93b664d7daa0d08b70
채상희 <chaesanghui142@gmail.com>
2024-06-26 21:42:32 +0900

    feat: "유효성 검사 util 구현 및 필터 적용"

---
+++ src/main/java/com/nhnacademy/apigateway/filter/JwtAuthenticationGlobalFilter.java
@@ -0,0 +1,49 @@
+package com.nhnacademy.apigateway.filter;
+
+import com.nhnacademy.apigateway.util.JwtUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthenticationGlobalFilter implements GlobalFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationGlobalFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getPath().value();
+
+        // "/auth/login" 경로는 제외
+        if (path.startsWith("/auth/login")) {
+            return chain.filter(exchange);
+        }
+
+        String authorizationHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+
+            if (jwtUtil.isTokenValid(token)) {
+                // 토큰이 유효한 경우
+                return chain.filter(exchange);
+            } else {
+                // 토큰이 유효하지 않은 경우
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+        }
+
+        // Authorization 헤더가 없거나 Bearer 토큰이 아닌 경우
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+}
