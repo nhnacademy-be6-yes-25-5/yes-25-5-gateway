d8badf090ba03d234bf53151eb6d1a3594502e36
bbbbooo <qlqlzbxkzl@naver.com>
2024-06-26 17:23:37 +0900

    fix : 모든 출처에 대해 허용하도록 수정함

--- src/main/java/com/nhnacademy/apigateway/config/CorsConfig.java
+++ src/main/java/com/nhnacademy/apigateway/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("http://localhost:8040");
+        corsConfig.addAllowedOrigin("*");
         corsConfig.addAllowedMethod("*");
         corsConfig.addAllowedHeader("*");
         corsConfig.setAllowCredentials(true);
