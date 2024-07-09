ebde5a90f9ea3f4ae88d29ced1d93d43705074a8
채상희 <chaesanghui142@gmail.com>
2024-07-09 19:27:35 +0900

    feat: "AuthAdaptor url 수정"

--- src/main/java/com/nhnacademy/apigateway/infrastructure/adaptor/AuthAdaptor.java
+++ src/main/java/com/nhnacademy/apigateway/infrastructure/adaptor/AuthAdaptor.java
@@ -6,10 +6,10 @@ import com.nhnacademy.apigateway.presentation.dto.response.AuthResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "authAdaptor", url = "${api.authority-server}")
+@FeignClient(name = "authAdaptor", url = "${api.authority-server}/refresh")
 public interface AuthAdaptor {
 
-    @GetMapping("/refresh")
+    @GetMapping
     ResponseEntity<AuthResponse> refreshAccessToken(@RequestHeader("Refresh-Token") String refreshJwt);
 
 }
